18bc5934e1c48b3fc96c9ba9f0579ba3d94fb77f
srongklod <klod9288@gmail.com>
2019-08-21 14:57:32 +0700

    getActivity false

--- app/src/main/java/srongklod_bangtamruat/p/TutorJava/MainActivity.java
+++ app/src/main/java/srongklod_bangtamruat/p/TutorJava/MainActivity.java
@@ -9,5 +9,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().add(R.id.FrameLayoutMain, new introduction()).commit();
+        }
+    }//Main Method
+
+}//Main Class
--- app/src/main/java/srongklod_bangtamruat/p/TutorJava/introduction.java
+++ app/src/main/java/srongklod_bangtamruat/p/TutorJava/introduction.java
@@ -1,7 +1,47 @@
 package srongklod_bangtamruat.p.TutorJava;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class introduction extends Fragment {
-    
-}
+    private TextView textView;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        InitialView();
+        TextViewOnClick();
+
+    }//Main Method
+
+    private void TextViewOnClick() {
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                    getActivity().getSupportFragmentManager().beginTransaction()
+                            .replace(R.id.FrameLayoutMain, new Content()).addToBackStack(null).commit();
+
+            }
+        });
+    }
+
+    private void InitialView() {
+        textView = getView().findViewById(R.id.txtIntroduction);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.introduction, container, false);
+        return view;
+    }//onCreateView
+}//Main Class
